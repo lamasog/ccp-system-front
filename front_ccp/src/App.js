bf12776083561176b10c61b9116f400f730d78a5
D-Ono <hiromi.ono@unesp.br>
2021-08-04 15:57:43 -0300

    feat: add Input Component

--- front_ccp/src/App.js
+++ front_ccp/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 //import {ViewButton} from './components/ViewButton/index';<ViewButton >Funciona</ViewButton>
-import SideBar from './components/SideBar'
+//import SideBar from './components/SideBar'
+import {Input} from './components/Input/index'
 import GlobalStyles from './style/GlobalStyle';
 
 function App() {
   return (
     <div>
-      <SideBar></SideBar>
+      <Input placeholder="Teste"></Input>
       <GlobalStyles></GlobalStyles>
     </div>
   );
---
+++ front_ccp/src/components/Input/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+    width: 330px;
+    height: 40px;
+    background: #F7F3F3;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    margin: 30px;
+    text-indent: 15px;
+    outline: 0;
+`;
