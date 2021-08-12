2ecdd6fc0059b1b43fea97a21cd7f27dad416514
Pedro H. Z. Costa <67840448+ZagoPedro@users.noreply.github.com>
2021-08-12 11:12:00 -0300

    paginação inicial

---
+++ front_ccp/src/pages/FormPage/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    height: 100vh;
+`;
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const Form = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
