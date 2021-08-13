2a870b35e55f7ab3abeb478fc6ce5376394f546c
D-Ono <hiromi.ono@unesp.br>
2021-08-13 18:13:53 -0300

    fix: routes

--- front_ccp/src/App.js
+++ front_ccp/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-//import {ViewButton} from './components/ViewButton/index';<ViewButton >Funciona</ViewButton>
-//import SideBar from './components/SideBar'
 import Webpages from './pages/index'
 import GlobalStyles from './style/GlobalStyle';
 
