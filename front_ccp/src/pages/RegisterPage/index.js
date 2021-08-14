78281a581d1c9539f7f1e3a1c310c9adf144c3dd
D-Ono <hiromi.ono@unesp.br>
2021-08-14 11:43:17 -0300

    feat: add Register Page

--- front_ccp/src/pages/RegisterPage/index.js
+++ front_ccp/src/pages/RegisterPage/index.js
@@ -1,28 +1,54 @@
 import React from 'react';
-import {ContainerPage, ContainerMain, ContainerRegister} from './style'
+import {ContainerPage, ContainerMain, ContainerRegister, RadioContainer} from './style'
 import {Input} from '../../components/Input/index'
 import { Link } from 'react-router-dom';
-import SideBar from '../../components/SideBar'
+import {SideBarContainer} from '../../components/SideBar'
+import Folder from '../../assets/Folder.svg' 
 
 function Register() {
 
   return (
     <ContainerPage>
         <ContainerMain>
-            <SideBar></SideBar>
+            <SideBarContainer>
+                <h1>Sistema USP</h1>
+                <div>
+                    <h2>Já tem uma conta?</h2>
+                    <Link exact to="/"><h3>Faça o Login Aqui!</h3></Link>
+                </div>
+                <img src={Folder} alt="Folder"></img>
+            </SideBarContainer>
             <ContainerRegister>
-                <h1>Login</h1>
+                <h1>Registre-se</h1>
                 <form>
                     <Input 
-                        placeholder="Nome do Usuario"
+                        placeholder="Nome Completo"
                         type="name"
                     />
                     <Input 
-                        placeholder="Senha" 
-                        type="password"
+                        placeholder="Matricula"
+                        type="name"
+                    />
+                    <Input 
+                        placeholder="E-Mail" 
+                        type="email"
                         required
                     />
-                    <button className="buttonSubmit" type="submit"><Link to="/">Entrar</Link></button>
+                    <Input 
+                        placeholder="Senha"
+                        type="password"
+                    />
+                    <RadioContainer>
+                        <div>
+                            <input type="radio" id="Professor" name="drone" value="Professor"/>
+                            <label for="Professor">Professor</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="Aluno" name="drone" value="Aluno  "/>
+                            <label for="Aluno">Aluno</label>
+                        </div>
+                    </RadioContainer>
+                    <Link to="/"><button className="buttonSubmit" type="submit">CADASTRAR</button></Link>
                 </form>
             </ContainerRegister>
         </ContainerMain>        
--- front_ccp/src/pages/RegisterPage/style.js
+++ front_ccp/src/pages/RegisterPage/style.js
@@ -13,19 +13,21 @@ export const ContainerMain = styled.div`
   background-color: #FFFFFF;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
   align-items: center;
 `;
 
 export const ContainerRegister = styled.div`
   width: 70%;
-  height: 70vh;
+  height: 100vh;
   margin-top: 5rem;
   margin-bottom: 6rem;
   display:flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
+  h1{
+    margin: 5rem;
+  }
   .buttonSubmit {
     width: 20rem;
     height: 5rem;
@@ -47,8 +49,19 @@ export const ContainerRegister = styled.div`
   }
   form {
     width: 100%;
+    height: 60%;
     display: flex;
     flex-direction: column;
+    justify-content: space-around;
     align-items: center;
   }
+`;
+
+export const RadioContainer = styled.div`
+  color: black;
+  width: 30%;
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 `;
