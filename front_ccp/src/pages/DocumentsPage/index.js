f9bd5b47162bbe2316f3a4087b2254f78b5634ea
D-Ono <hiromi.ono@unesp.br>
2021-09-08 10:58:34 -0300

    feat: add pagina com docx dos alunos

---
+++ front_ccp/src/pages/DocumentsPage/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { SideBarContainer } from "../../components/SideBar";
+import Folder from "../../assets/Home.svg";
+import { ContainerPage, ContainerMain, Header, Content } from "./style";
+import Exit2 from "../../assets/Exit2.svg";
+
+const DocumentsPage = () => {
+	return (
+		<ContainerPage>
+			<SideBarContainer>
+				<img src={Folder} alt="Folder"></img>
+			</SideBarContainer>
+			<ContainerMain>
+				<Header>
+					<div className="ContainerHeaderText">
+						<span className="headerText">Documentos dos Alunos</span>
+					</div>
+					<button className="buttonExit" type="submit">
+						Sair
+						<img className="imgExit" src={Exit2} alt="Exit"></img>
+					</button>
+				</Header>
+				<hr />
+				<Content>
+					<div className="containerContentInput">
+						
+					</div>
+				</Content>
+			</ContainerMain>
+		</ContainerPage>
+	);
+};
+
+export default DocumentsPage;
